dc29bc7ef0645691ec5faaba675972136cdfa223
neurovix <neurovix.nv@gmail.com>
2025-10-21 23:38:30 -0600

    android prod test #2

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout() {
     <AuthProvider>
       <ThemeProvider value={DefaultTheme}>
         <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="(hidden)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" options={{ headerShown: false }} />
